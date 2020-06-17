4ace64848e9e8ad431a413e46c3bcec763c06561
kamil kawalkiewicz <kamil.kawalkiewicz@gmail.com>
2020-06-17 11:36:39 +0200

    add NotesPHP

---
+++ README.md
@@ -0,0 +1,29 @@
+#NotesPHP
+
+## General info
+
+Its NotesPHP.
+
+In this project i can learning basic of PHP.
+This application based on CRUD and MVC. So you can create, read, update and delete your notes and its 
+cooperates with data base MySQL
+
+## Technologies
+
+Project is created with:
+
+- HTML
+- CSS
+- PHP
+- Mysql
+- SQL
+- CRUD
+- MVC
+
+## Status
+
+Project is finished
+
+## Contact
+
+kamil.kawalkiewicz@gmail.com
---
+++ templates/pages/show.php
@@ -0,0 +1,20 @@
+<div class="show">
+  <?php $note = $params['note'] ?? null; ?>
+  <?php if ($note) : ?>
+    <ul>
+      <li>Id: <?php echo (int) $note['id'] ?></li>
+      <li>Tytuł: <?php echo $note['title'] ?></li>
+      <li>
+        <pre>Treść: <?php echo $note['description'] ?></pre>
+      </li>
+      <li>Zapisano: <?php echo $note['created'] ?></li>
+    </ul>
+    <a href="/?action=edit&id=<?php echo $note["id"] ?>"><button>Edytuj</button></a>
+    
+  <?php else : ?>
+    <div>Brak notatki do wyświetlenia</div>
+  <?php endif; ?>
+  <a href="/">
+    <button>Powrót do listy notatek</button>
+  </a>
+</div>
